ed17cf0f2585767ce5c2acdc3f72b54405ee9ec8
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2022-12-17 01:35:24 +0300

    Components, Firbase and stack nav added

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import { StyleSheet, Text, View, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+//import { createDrawerNavigator } from "@react-navigation/drawer";
+import Home from "./Components/Home";
+import Add from "./Components/Add";
+import Login from "./Components/Login";
+import { Entypo } from "@expo/vector-icons";
+import { Sign } from "./Components/Sign";
+import { useNavigation } from "@react-navigation/native";
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Home
+      nav={() => navigation.navigate("Add")}
+      login={() => navigation.navigate("Login")}
+    />
+  );
+}
+function AddScreen() {
+  return <Add />;
+}
+function LoginScreen() {
+  return <Login login={() => navigation.push("Sign")} />;
+}
+function SignScreen() {
+  return <Sign />;
+}
+
+const Stack = createNativeStackNavigator();
+//const Drawer = createDrawerNavigator();
+export default function App() {
+  //const n = useNavigation();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#6495ed",
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "bold",
+            fontSize: 35,
+            fontWeight: "900",
+          },
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Andika", headerShown: false }}
+        />
+        <Stack.Screen name="Add" component={AddScreen} />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ title: "Andika", headerShown: false }}
+        />
+        <Stack.Screen
+          name="Sign"
+          component={SignScreen}
+          options={{ title: "Register", headerShown: true }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: {},
 });
